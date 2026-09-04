5593e71570d015cd82d18975a732f871762e208b
JJ-Martinez-Garcia <tutank@gmail.com>
2026-09-04 20:51:10 +0200

    Refresh service worker cache for v43

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "archaeocad-mobile-v42";
+const CACHE_NAME = "archaeocad-mobile-v43";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/favicon.svg", "/icon-192.png", "/icon-512.png", "/apple-touch-icon.png"];
 
 self.addEventListener("install", (event) => {
